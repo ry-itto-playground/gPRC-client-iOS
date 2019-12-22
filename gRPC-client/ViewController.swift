0540f93d0e23711bcff88d20369bd060129588bd
ryoya ito <horusu.dqx@gmail.com>
2019-12-22 12:48:04 +0900

    :tada: initialize project

---
+++ gRPC-client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gRPC-client
+//
+//  Created by 伊藤凌也 on 2019/12/22.
+//  Copyright © 2019 ry-itto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
